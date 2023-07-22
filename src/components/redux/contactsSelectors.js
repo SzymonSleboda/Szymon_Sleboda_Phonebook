3c84e7e774c3612cee955092d4b608ecb9167b71
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-22 12:38:35 +0200

    authSlice added

--- src/components/redux/contactsSelectors.js
+++ src/components/redux/contactsSelectors.js
@@ -1,7 +1,14 @@
-export const getFilteredContacts = state => {
-    const { contacts, filter } = state;
-    const optimizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(optimizedFilter)
-    );
-  };
+export const getFilteredContacts = (contacts, filter) => {
+  const normalizedFilter = filter.toLowerCase();
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+};
+
+export const getFilter = state => state.filter;
+
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const getUserName = state => state.auth.user.name;
+
+export const getToken = state => state.auth.token;

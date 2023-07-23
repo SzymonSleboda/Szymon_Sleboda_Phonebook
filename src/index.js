14d23e21f616f1fcf7f582d0e77ef8dd1caaf29b
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-23 13:09:30 +0200

    added store.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import App from './components/App';
-import contactsReducer from './components/redux/contactsSlice'; 
-import filterReducer from './components/redux/filterSlice'; 
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from 'redux/store';
+import App from 'components/App';
 import './index.css';
 
-const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
-});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );

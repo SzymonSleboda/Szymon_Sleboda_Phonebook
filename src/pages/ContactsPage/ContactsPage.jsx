226d32c4f29b10f06ef1ac36219e2899873c249a
SzymonSleboda <szymonskeboda@gmail.com>
2023-07-23 20:50:07 +0200

    added pages

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+
+import ContactForm from 'components/ContactForm/';
+import Filter from 'components/Filter';
+import ContactList from 'components/ContactList';
+import { getIsLoggedIn } from 'redux/contactsSelectors';
+
+import s from './ContactsPage.module.css';
+
+const ContactsPage = () => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  return (
+    isLoggedIn && (
+      <div className={s.wrapper}>
+        <h1 className={s.titlePhone}>Phonebook</h1>
+        <ContactForm />
+        <h2 className={s.titleCont}>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    )
+  );
+};
+export default ContactsPage;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { getIsLoggedIn } from 'redux/contactsSelectors';
+import s from './HomePage.module.css';
+
+const HomePage = () => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  return (
+    <div className={s.wrapper}>
+      <h1>Welcome to your Phonebook!</h1>
+      <p>
+        Welcome to Phone Book, the ultimate app for managing your contacts! With
+        Phone Book, you can easily create a new user account and login to start
+        organizing your contacts right away.
+      </p>
+      <p>
+        So why wait? Sign up for Phone Book today and start organizing your
+        contacts like a pro!
+      </p>
+      {!isLoggedIn && (
+        <>
+          <p className={s.text}>
+            Have an account?
+            <span className={s.pre_text}>
+              <Link to="/login" className={s.link}>
+                Login
+              </Link>
+            </span>
+          </p>
+          <p className={s.text}>
+            No account?
+            <span className={s.pre_text}>
+              <Link to="/register" className={s.link}>
+                Sig In!
+              </Link>
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+export default HomePage;
